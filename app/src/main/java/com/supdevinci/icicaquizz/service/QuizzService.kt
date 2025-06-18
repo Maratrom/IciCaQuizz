9e790335a8557f18b60a6c3720d6c8bb81acff95
Maratrom <arthur.lagneaux@gmail.com>
2025-06-18 11:23:57 +0200

    Rework QuizzService to include parameters

--- app/src/main/java/com/supdevinci/icicaquizz/service/QuizzService.kt
+++ app/src/main/java/com/supdevinci/icicaquizz/service/QuizzService.kt
@@ -1,9 +1,15 @@
 package com.supdevinci.icicaquizz.service
 
 import com.supdevinci.icicaquizz.model.QuizzResponse
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface QuizzService {
-    @GET("api.php?amount=1")
-    suspend fun getQuizz(): QuizzResponse
+    @GET("api.php")
+    suspend fun getQuizz(
+        @Query("amount") amount: Int,
+        @Query("category") category: Int? = null,
+        @Query("difficulty") difficulty: String? = null
+    ): Call<QuizzResponse>
 }
